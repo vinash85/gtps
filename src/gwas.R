4e89e1fcfbe035845eba2fff1d6db7f25c220ead
avinash sahu <avinash.sahu@gmail.com>
2014-11-05 14:01:46 -0500

    initial checkin

---
+++ src/gwas.R
@@ -0,0 +1,8 @@
+iter <- ncol(obj$x)
+iters <- 1:iter
+#iters <- eeSNP
+eQTL <- mclapply(iters ,  function(xx) {
+		       aa = cor.test(obj$x[,xx], obj$y[, obj$mask2[xx,1]]);
+		       return(c(aa$p.value, aa$estimate))
+}, mc.cores=1)
+eqtl <- t(do.call(cbind, eQTL))
